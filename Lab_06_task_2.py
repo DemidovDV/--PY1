6c57a37fe58f482e24cfd12ff2100353ecdbe11a
DemidovDV <115521222+DemidovDV@users.noreply.github.com>
2022-11-15 17:11:04 +0300

    Лабораторная работа 6

---
+++ Lab_06_task_2.py
@@ -0,0 +1,21 @@
+import json
+
+INPUT_FILE = "output.csv"
+
+def csv_to_list_dict(filename):
+    data = []
+    result = []
+    with open(filename) as csv_file:
+        for line in csv_file:
+            data.append(line.split(','))
+
+        for i in range(1, len(data)):
+            row = {}
+            for j in range(len(data[0])):
+                row[data[0][j]] = data[i][j].rstrip('\n')
+
+            result.append(row)
+
+    return result
+
+print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
